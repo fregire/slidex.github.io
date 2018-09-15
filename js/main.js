b930c68acce091e9a193b2d5130689fdbff03699
fregire <fregire@mail.ru>
2018-09-15 20:35:11 +0500

    Создал и внедрил функцию видимости элемента при скролле страницы

--- js/main.js
+++ js/main.js
@@ -1,18 +1,18 @@
 $(document).ready(function(){
+	var screenHeight = screen.height;
+	// Функция - видим ли элемент при скролле вниз
+	function isVisible(elem, screenHeight){
+		var elemTopPosition = elem.getBoundingClientRect().top;
+		var result = elemTopPosition < screenHeight - 200;
+		
+		return result;
+	}
 	// Появление картинки проектов при скролле
 	$(window).scroll(function(){
-		var $elem = $(".about__photo");
-		var windowBottomCoord = $(window).scrollTop() + $(window).height();
-		// console.log("Window - " + ($elem.offset().top - $(window).scrollTop()));
-		// console.log("Elem - " + $elem.offset().top);
-		var top_of_element = $(".about__photo").offset().top;
-	    var bottom_of_element = $(".about__photo").offset().top + $(".about__photo").outerHeight();
-	    var bottom_of_screen = $(window).scrollTop() + window.innerHeight;
-	    var top_of_screen = $(window).scrollTop();
-
-	   	if($(window).scrollTop() > 400){
-	   		$elem.addClass("about__photo--visible");
-	   	}
+		var elem = document.querySelector(".about__photos");
+		if(isVisible(elem, screenHeight)){
+			$(".about__photo").addClass("about__photo--visible");
+		}
 	});
 
 	$(".about__text").click(function(){
@@ -20,15 +20,11 @@ $(document).ready(function(){
 	});
 
 
-	var screenHeight = screen.height;
 	$(window).scroll(function(){
 		var topBorderElems = document.querySelectorAll(".unknown__item");
-		var elemTopPosition;
+
 		for(var i = 0; i < topBorderElems.length; i++){
-			elemTopPosition = topBorderElems[i].getBoundingClientRect().top;
-			if(elemTopPosition > screenHeight - 100){
-				continue;
-			} else {
+			if(isVisible(topBorderElems[i], screenHeight)){
 				topBorderElems[i].classList.add("unknown__item--visible");
 			}
 		}
