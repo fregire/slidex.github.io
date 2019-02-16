7472141c6d6a93ef1b43af34b3ca6cfa053234d1
fregire <fregire@mail.ru>
2019-02-16 20:31:53 +0500

    Изменил мин. дистанцию свайпа

--- js/singleProjects.js
+++ js/singleProjects.js
@@ -19,7 +19,7 @@
 	var startX, 
 			startTime, 
 			maxTime = 300,
-			minDist = 50,
+			minDist = 70,
 			swipeDir = "none",
 			pageArrowPrevHref = document.querySelector(".page-nav--prev").href,
 			pageArrowNextHref = document.querySelector(".page-nav--next").href;
